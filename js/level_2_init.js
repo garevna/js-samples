239bdf7655e02ef90ae4b5cc60a49dac1fc07aa3
garevna <garevna@mail.ru>
2017-11-15 14:56:58 +0200

    Add files via upload

---
+++ js/level_2_init.js
@@ -0,0 +1,24 @@
+document.scriptsRegistry = new GarevnaScriptsRegistry ()
+document.stylesheetsRegistry = new GarevnaStylesRegistry ()
+document.worker = new Worker ( '/snippets/level_2_registry_worker.js' )
+console.info ( '--- level_2_init works ---' )
+document.worker.onmessage = function ( mess ) {
+	console.info ( 'level_2_registry_worker received data: type = ' + mess.data.type + '; src = ' + mess.data.src )
+	if ( mess.data.type == 'script' ) {
+		console.log ( '#### level_2_init received SCRIPT data from level_2_registry_worker: ', mess.data.src )
+		document.scriptsRegistry.registerScript ( {
+					src: mess.data.src,
+					external: true,
+					functions: mess.data.functions || [],
+					defer: mess.data.defer || false,
+					async: mess.data.async || true,
+		} )
+	} else {
+		console.log ( 'RECEIVED STYLESHEET: ', mess.data )
+		document.stylesheetsRegistry.registerStyleSheet ( mess.data )
+	}
+}
+console.info ( '===================== Реестр скриптов ==========================' )
+console.log ( document.scriptsRegistry )
+console.info ( '===================== Реестр таблиц стилей ==========================' )
+console.log ( document.stylesheetsRegistry )
