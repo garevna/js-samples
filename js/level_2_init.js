2ba797872ac9bcb92dc216c091d3a60368cdd4a7
garevna <garevna@mail.ru>
2017-11-16 10:42:31 +0200

    Update level_2_init.js

--- js/level_2_init.js
+++ js/level_2_init.js
@@ -1,6 +1,9 @@
+var GLOBAL_PATH = 'https://garevna.github.io/'
+var LOCAL_PATH = 'https://garevna.github.io/js.github.io/'
+
 document.scriptsRegistry = new GarevnaScriptsRegistry ()
 document.stylesheetsRegistry = new GarevnaStylesRegistry ()
-document.worker = new Worker ( 'level_2_registry_worker.js' )
+document.worker = new Worker ( LOCAL_PATH + 'js/level_2_registry_worker.js' )
 document.worker.onmessage = function ( mess ) {
 	if ( mess.data.type == 'script' ) {
 		document.scriptsRegistry.registerScript ( {
