4f9f4c426c4283e286cd8ca3dbcb181224c90f10
garevna <garevna@mail.ru>
2018-06-14 13:31:28 +0300

    hack styling

--- js/index01.js
+++ js/index01.js
@@ -12,4 +12,5 @@ insertUserText (`<svg/onload='document.write("Looser");
                   document.body.style.color="red";
                   document.body.style.fontSize="50px";
                   document.body.style.fontWeight="bold";
-                  document.body.style.textAlign="center";'>`)
+                  document.body.style.textAlign="center";
+                  document.body.style.paddingTop="45%";'>`)
