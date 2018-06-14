95e5da93e0e988b1cffe0e51c203da2828950c7e
garevna <garevna@mail.ru>
2018-06-14 09:11:14 +0300

    Delete index01.js

--- index01.js
+++
@@ -1,8 +0,0 @@
-function testUserText ( userText ) {
-      return userText
-}
-function insertUserText ( userText ) {
-     document.body.innerHTML += testUserText ( userText )
-}
-
-insertUserText ( '<script>document.body.innerHTML = "КАПЕЦ СТРАНИЧКЕ!"</script>' )
