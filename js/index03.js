8da246e6d5775f2a553403253f77ba1a1cb893c6
garevna <garevna@mail.ru>
2018-06-14 15:54:31 +0300

    Update index03.js

--- js/index03.js
+++ js/index03.js
@@ -1,16 +1,12 @@
-var sample = {
-        elem: document.querySelector ( 'input' ),
-        getElemContent: () => this.elem.value,
-        testElemContent: () => 
-            this.elem.value.split("document").join(""),
-        showElemContent: () => {
-                document.write ( this.testElemContent () )
-        }
+var sample = document.querySelector ( 'input' )
+
+function testElemContent () {
+        return sample.value.split("document").join("")
+}
+sample.onchange =  function () {
+        document.write ( testElemContent() )
 }
 
-sample.elem.onchange = function ( event ) {
-        sample.showElemContent ()
-}
 
 // <IMG SRC=/ onerror="alert(String.fromCharCode(88,83,83))"></img>
 // <BODY BACKGROUND="javascript:alert('XSS')">
