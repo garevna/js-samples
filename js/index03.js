c7e5d1f6233f8dd05ecd4f3f744a6a7860da8c9c
garevna <garevna@mail.ru>
2018-06-14 16:26:56 +0300

    Update index03.js

--- js/index03.js
+++ js/index03.js
@@ -1,12 +1,15 @@
-var sample = document.querySelector ( 'input' )
-
-function testElemContent () {
-        return sample.value.split("document").join("")
+function testUserText ( userText ) {
+      return userText
 }
-sample.onchange =  function () {
-        document.body.innerHTML += testElemContent()
+function insertUserText ( userText ) {
+      var x = document.createElement ( 'div' )
+      x.innerHTML = testUserText ( userText )
+      document.body.appendChild ( x )
 }
 
-
+document.querySelector ( 'input' ).onchange = function ( event ) {
+        console.log ( event.target.value )
+        insertUserText ( event.target.value )
+}
 // <IMG SRC=/ onerror="alert(String.fromCharCode(88,83,83))"></img>
 // <BODY BACKGROUND="javascript:alert('XSS')">
