f2f970106ae31efde6e14d48042e0b2a7a7c5c11
garevna <garevna@mail.ru>
2018-06-15 18:08:49 +0300

    Update index08.js

--- js/index08.js
+++ js/index08.js
@@ -1,23 +1,42 @@
 var script = document.createElement ( 'script' )
 script.src = "https://cdn.rawgit.com/chrisveness/crypto/4e93a4d/sha256.js"
-script.onload = function ( event ) {
-    var hash = ""
-    fetch ( 'js/testSHA.js' )
+
+script.onload = event => shaButton.disabled = false
+script.onerror = event => 
+            console.error ( "Ошибка загрузки скрипта " + script.src )
+
+document.head.appendChild ( script )
+
+var hash = document.createElement ( 'p' )
+document.body.appendChild ( hash )
+hash.innerHTML = "You should input file name"
+
+var shaButton = document.createElement ( 'button' )
+document.body.appendChild ( shaButton )
+shaButton.innerHTML = "SHA256 hash for file content"
+shaButton.disabled = true
+shaButton.onclick = event => {
+    var fileName = document.querySelector ( 'input' ).value || 'js/testSHA.js'
+    document.querySelector ( 'input' ).value = fileName
+    fetch ( fileName )
         .then ( responseObject => {
                 responseObject.text().then ( response => {
-                        console.log ( 'TEXT: ', response )
-                        hash = Sha256.hash ( response )
-                        console.log ( 'HASH: ', hash )
+                        hash.innerHTML = Sha256.hash ( response )
+                        loadButton.disabled = false
                 })
         })
-    //var userScript = document.createElement ( 'script' )
-    //userScript.src = "js/testSHA.js"
-    //userScript.integrity = "sha256-7bb9fbc3a068bc2751364d8269f7b64cbcbaeb463ed5dec238c71abeead6bda2"
-    //userScript.id = "sha256Test"
-    //userScript.onerror = () => console.log ( "Integrity test has failed" )
-    //document.head.appendChild ( userScript )
 }
-script.onerror = function ( event ) {
-    console.error ( "Ошибка загрузки скрипта " + script.src )
+
+var loadButton = document.createElement ( 'button' )
+document.body.appendChild ( loadButton )
+loadButton.innerHTML = "load script file with integrity"
+loadButton.disabled = true
+loadButton.onclick = event => {
+    var userScript = document.createElement ( 'script' )
+    userScript.src = "js/testSHA.js"
+    userScript.integrity = "sha256-" + hash.innerHTML
+    userScript.onerror = () => console.log ( "Integrity test has failed" )
+    document.head.appendChild ( userScript )
 }
-document.head.appendChild ( script )
+
+
