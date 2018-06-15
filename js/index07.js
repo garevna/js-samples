f081d01aed7fc65b412624c23fd478ea4a741930
garevna <garevna@mail.ru>
2018-06-15 12:15:00 +0300

    Update index07.js

--- js/index07.js
+++ js/index07.js
@@ -1,7 +1,11 @@
 var script = document.createElement ( 'script' )
 script.src = "https://cdn.rawgit.com/chrisveness/crypto/4e93a4d/sha256.js"
+script.onload = function ( event ) {
+    var sample = "Привет всем студентам A-level"
+    var hash = Sha256.hash ( sample )
+    console.log ( hash )
+}
+script.onerror = function ( event ) {
+    console.error ( "Ошибка загрузки скрипта " + script.src )
+}
 document.head.appendChild ( script )
-
-var sample = "Привет всем студентам A-level"
-var hash = Sha256.hash ( sample )
-console.log ( hash )
