a55ca7fc77eede3c49977d87dc961a867ed236f7
garevna <garevna@mail.ru>
2018-06-15 10:41:09 +0300

    Update resolve05.js

--- docs/resolve05.js
+++ docs/resolve05.js
@@ -2,9 +2,6 @@ function getCurrentPostComments ( postId ) {
         var res = []
         for ( var x of comments ) {
                 if ( x.postId === postId ) {
-                        for ( var user of users ) {
-                                
-                        }
                         var user = -1
                         var index = 0
                         do {
