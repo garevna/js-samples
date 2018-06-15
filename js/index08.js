03a035d93762703d477a55f8941a18654c5632ab
garevna <garevna@mail.ru>
2018-06-15 13:50:40 +0300

    Update index08.js

--- js/index08.js
+++ js/index08.js
@@ -1,6 +1,15 @@
 var script = document.createElement ( 'script' )
 script.src = "https://cdn.rawgit.com/chrisveness/crypto/4e93a4d/sha256.js"
 script.onload = function ( event ) {
+    var hash = ""
+    fetch ( 'js/testSHA.js' )
+        .then ( responseObject => {
+                responseObject.text().then ( response => {
+                        console.log ( 'TEXT: ', response )
+                        hash = Sha256.hash ( response )
+                        console.log ( 'HASH: ', hash )
+                })
+        })
     var userScript = document.createElement ( 'script' )
     userScript.src = "js/testSHA.js"
     userScript.integrity = "sha256-7bb9fbc3a068bc2751364d8269f7b64cbcbaeb463ed5dec238c71abeead6bda2"
