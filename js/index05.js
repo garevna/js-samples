8fb666faf71434c921972b2454839f43c2369842
garevna <garevna@mail.ru>
2018-06-15 10:23:04 +0300

    Update index05.js

--- js/index05.js
+++ js/index05.js
@@ -72,25 +72,7 @@ var comments = [
 
 function getCurrentPostComments ( postId ) {
         var res = []
-        for ( var x of comments ) {
-                if ( x.postId === postId ) {
-                        for ( var user of users ) {
-                                
-                        }
-                        var user = -1
-                        var index = 0
-                        do {
-                                user = users [ index ].userId === x.author ? index : user
-                                console.log ( 'user: ', user, ' index: ', index, ' users.length: ', users.length )
-                                index += 1
-                        }
-                        while ( index < users.length )
-                        res.push ({
-                                autor: user < 0 ? "anonymous" : users [ user ] .name,
-                                text: x.text
-                        })
-                }
-        }
+        
         return res
 }
 
