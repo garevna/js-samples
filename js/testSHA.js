cef67d1db085e50afc0fdf86f9bc6ddcb7834084
garevna <garevna@mail.ru>
2018-06-28 13:07:48 +0300

    Update testSHA.js

--- js/testSHA.js
+++ js/testSHA.js
@@ -4,7 +4,9 @@ hello.style = `position: fixed;
                 bottom: 15%; right: 15%; 
                 box-shadow: 10px 10px 16px #00000090; 
                 border: solid 0.5px #bbb; 
-                padding: 30px;`
+                padding: 30px;
+                z-index: 300;
+                background-color: #eef;`
 document.body.appendChild ( hello )
 hello.innerHTML = "<h3>Success!</h3>"
 hello.innerHTML += "<p>file integrity test resuts with OK</p>"
