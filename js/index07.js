ca92c0c2a1c7a08cf1dfc3c2c85b7037cdb124a0
garevna <garevna@mail.ru>
2018-06-28 20:17:57 +0300

    Update index07.js

--- js/index07.js
+++ js/index07.js
@@ -1,11 +1,21 @@
+var sample = document.querySelector ( "input" )
+sample.oninput = "getTextDigest ( this.value )"
+sample.value = "Привет студентам A-level"
+
+var demo = document.createElement ( 'p' )
+document.body.appendChild ( demo )
+
 var script = document.createElement ( 'script' )
 script.src = "https://cdn.rawgit.com/chrisveness/crypto/4e93a4d/sha256.js"
 script.onload = function ( event ) {
-    var sample = "Привет всем студентам A-level"
-    var hash = Sha256.hash ( sample )
-    console.log ( hash )
+    getTextDigest ( sample.value )
 }
 script.onerror = function ( event ) {
     console.error ( "Ошибка загрузки скрипта " + script.src )
 }
 document.head.appendChild ( script )
+
+function getTextDigest ( text ) {
+    var hash = Sha256.hash ( text )
+    demo.innerHTML = `Text: ${sample} Hash: ${hash}`
+}
