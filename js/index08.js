d379a321415f11b418970bb0638295685698b066
garevna <garevna@mail.ru>
2018-06-28 11:58:32 +0300

    Update index08.js

--- js/index08.js
+++ js/index08.js
@@ -26,7 +26,9 @@ shaButton.onclick = event => {
          crossorigin: "anonymous"
     } )
     .then ( responseObject => {
-                responseObject.clone().then ( response => {
+                responseObject.text().then ( response => {
+                            response = response.split(" ").join("")
+                            console.log ( response )
                         hash.innerHTML = Sha256.hash ( response )
                         loadButton.disabled = false
                         document.querySelector ( 'input' ).disabled = true
