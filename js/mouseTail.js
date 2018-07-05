28696c58fcf085624778acbbd5fd93b26ca668a2
garevna <garevna@mail.ru>
2018-07-05 10:24:13 +0300

    Update mouseTail.js

--- js/mouseTail.js
+++ js/mouseTail.js
@@ -1,14 +1,16 @@
 function createMouseTail ( text ) {
-     document.body.style = `
+     var parentElem = document.createElement ( 'div' )
+     document.body.insertBefore ( parentElem, document.body.firstChild )
+     parentElem.style = `
      position: fixed;
      top: 0;
      bottom: 0;
      left: 0;
      right: 0;
+     z-index:-1;
      background-color: #000;
-     color: #ddd;
     `
-     var shadow = document.body.attachShadow ( { mode: 'closed' } )
+     var shadow = parentElem.attachShadow ( { mode: 'closed' }
      shadow.appendChild (
         ( () => {
                 var style = document.createElement ( 'style' )
