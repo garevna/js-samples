c4403ca53bfa33323839089a89251a9b99c29b2b
garevna <garevna@mail.ru>
2018-07-10 10:25:52 +0300

    Search Object

--- js/index11.js
+++ js/index11.js
@@ -1,1 +1,16 @@
+location.search = '?name=garevna,date=10.07.2018'
 
+function getSearchObject () {
+        var obj = {}
+        location.search.slice(1).split(',')
+                .map ( x => x.split('=') )
+                .map ( function ( item ) {
+                        this[ item [0] ] = item [1]
+        }, obj )
+        return obj
+}
+
+var searchObject = getSearchObject ()
+for ( var rec in searchObject ) {
+        document.body.innerHTML += `<p>${rec}: ${searchObject[rec]}</p>`
+}
