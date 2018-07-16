a99b08b66969058232d2916d82da28636e28acde
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2018-07-16 15:18:58 +0300

    index13.js added

--- js/index13.js
+++ js/index13.js
@@ -1,1 +1,33 @@
+var tagNames = [ "div", "div", "div", "div", "button" ]
+var divStyle = `
+             display: inline-block;
+             width: 100px;
+             height: 100px;
+             border-radius: 50%;
+             border: solid 1px green;
+             font-size: 25px;
+        `
+elements = tagNames.map ( x => document.body.appendChild ( document.createElement ( x ) ) )
 
+elements.filter ( element => element.tagName === "DIV" )
+        .forEach ( ( element, num ) => {
+                        element.style = divStyle
+                        element.innerText = num
+                   })
+elements.filter ( element => element.tagName === "BUTTON" )
+        .forEach ( element => {
+                        element.innerHTML = "remove DIVs"
+                        element.onclick = function ( event ) {
+                                recursRemove ()
+                        }
+                   })
+
+var recursRemove = ( function ( selector ) {
+        var elems = document.querySelectorAll ( selector )
+        var num = 0
+        return function rem () {
+                if ( num === elems.length ) return
+                elems [ num ].parentNode.removeChild ( elems [ num++ ] )
+                rem ()
+        }
+}) ( "DIV" )
