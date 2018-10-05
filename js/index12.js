c9f1050110a5548230ead48bca7b465fc0e372b7
garevna <garevna@mail.ru>
2018-10-05 12:12:18 +0300

    Update index12.js

--- js/index12.js
+++ js/index12.js
@@ -1,15 +1,28 @@
 var User = function ( name = "user",
                       email = "user@sample.com",
-                      photo = "https://vignette.wikia.nocookie.net/yogscast/images/8/8a/Avatar_Turps_2015.jpg" ) {
+                      photo = User.getAvatar() ) {
         this.name = name
         this.email = email
         this.photoURL = photo
         this.info = ''
 }
-User.__proto__.admin = {
+User.admin = {
         photoURL: "https://i.pinimg.com/originals/3d/47/4f/3d474f82ff71595e8081f9a120892ae8.gif",
         name: "admin"
 }
+User.getAvatar = function () {
+     return this.avatars.shift ()
+}
+User.avatars = [
+     "https://pre00.deviantart.net/50f9/th/pre/i/2011/217/e/8/pikachu_2_by_nostalgiaattack-d45jd3i.png",
+     "https://cdn.diversityavatars.com/wp-content/uploads/2018/01/Vector-Smart-Object-5.png",
+     "https://cdn4.iconfinder.com/data/icons/user-avatar-flat-icons/512/User_Avatar-31-512.png",
+     "http://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-L3-icon.png",
+     "https://findicons.com/files/icons/1072/face_avatars/300/i05.png",
+     "http://www.iconarchive.com/download/i51043/hopstarter/halloween-avatars/Gomez.ico",
+     "http://icons.iconarchive.com/icons/hopstarter/halloween-avatars/256/Zombie-2-icon.png",
+     "https://vignette.wikia.nocookie.net/yogscast/images/8/8a/Avatar_Turps_2015.jpg"
+]
 User.prototype.messageBox = ( function () {
         var box = document.createElement ( 'div' )
         document.body.appendChild ( box )
@@ -50,5 +63,19 @@ User.prototype.read = function () {
         this.messageBox.message.value = "OK"
 }
 
-var user1 = new User ( "Иван" )
-var user2 = new User ( 'Alex', "alex@gmail.com", 'https://78.media.tumblr.com/3a5c4d080cdd9c57139170b5015e244c/tumblr_oycng531hQ1vzkjvvo1_1280.jpg' )
+var users = []
+users.push ( new User ( "Иван" ) )
+users.push ( new User ( 'Alex', "alex@gmail.com" ) )
+users.push ( new User ( 'Bob', "bob777@gmail.com" ) )
+users.push ( new User ( 'Dima', "dima888@gmail.com" ) )
+users.push ( new User ( 'Fima', "fima999@gmail.com" ) )
+
+var k = 1
+users.forEach ( 
+     function ( user ) {
+          setTimeout ( 
+               function () {
+                    user.write ( `Hello, I'm ${user.name}` )
+               }, 3000 * k++
+          )
+} )
