1cf612ba2f1b1c6253776a28090c43b36d2c2a9e
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2019-02-05 20:53:08 +0200

    ES Modules ( sample 25 )

---
+++ js/index25.js
@@ -0,0 +1,8 @@
+let mod = document.body.appendChild (
+  document.createElement ( "script" )
+)
+mod.type = "module"
+mod.textContent = `
+    import {showMessage} from './js/testESModules.js';
+    showMessage ( "Hi, students! Welcome to new age of ES Modules!" )
+`
