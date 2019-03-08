3c8034fed759d3ec0650df731cc39dcbca067a9d
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2019-03-08 17:26:36 +0200

    sample 27

--- js/start.js
+++ js/start.js
@@ -3,6 +3,6 @@
      import ( `./index${location.hash.slice(1)}.js` )
      document.addEventListener(
        "DOMContentLoaded",
-       event => import ( `../__snippets/mouseTail.js` )
+       event => import ( `../snippets/mouseTail.js` )
      )
 })()
