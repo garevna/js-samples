00c75c8c4bfc660528e2d3c5e0b917d790d94665
garevna <garevna@mail.ru>
2019-06-26 13:35:04 +0300

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,48 +1,36 @@
-var addElem = ( tagName, container ) =>
-    ( container ? container : document.body )
-        .appendChild (
-            document.createElement ( tagName )
-        )
+const demo = document.querySelector ( '#demo' )
+const main = document.querySelector ( '#main' )
+const section_1 = document.querySelector ( '#section_1' )
+const section_2 = document.querySelector ( '#section_2' )
 
-var timeOut = time => new Promise (
-    resolve => setTimeout ( () => resolve(), time )
-)
 
-var container = addElem ( "div" )
-container.className = "flex-container"
-var startButton = addElem ( 'button', container )
-startButton.innerText = 'Крутить рулетку'
+function show ( event ) {
 
-startButton.onclick = event => {
-    var winnwer = Math.round ( Math.random() * 20000 )
-    var user
-    event.target.style.display = "none"
-    function show( photoURL, login ) {
-        img.src = photoURL
-        user = addElem ( "h4", container )
-        user.innerText = `winner: ${login}`
+  [ "scrollTop", "scrollLeft", "scrollHeight", "offsetTop", "offsetHeight", "offsetWidth", "clientHeight", "clientWidth" ].forEach (
+    item => {
+      this.querySelector( `#${event.target.id}_${item}` ).innerText = event.target[item]
     }
-    var img = addElem( "img", container )
-    img.height = "200"
-    img.src = "https://thumbs.gfycat.com/LivelyObviousAnhinga-size_restricted.gif"
-    img.style.transition = "all 0.5s"
-    timeOut ( 2500 )
-        .then ( () => img.src = "https://thumbs.gfycat.com/OddWideHookersealion-small.gif" )
-    timeOut ( 1800 )
-        .then ( () => img.style.opacity = 0 )
-    timeOut ( 2500 )
-        .then ( () => img.style.opacity = 1 )
-    timeOut ( 2500 )
-        .then (
-            () => fetch ( `https://api.github.com/users?since=${winnwer}` )
-                      .then ( response => response.json()
-                          .then ( users => show ( users[0].avatar_url, users[0].login ) )
-                      )
-        )
-    timeOut ( 10000 )
-        .then ( () => {
-            img.remove()
-            user.remove()
-            event.target.style.display = "block"
-        })
+  )
+  if ( event.target.id === "main" ) {
+          section_1.onscroll()
+          section_2.onscroll()
+  }
+}
+
+main.onscroll = show.bind ( demo.querySelector( "#main_demo" ) )
+section_1.onscroll = show.bind ( demo.querySelector( "#section_1_demo" ) )
+section_2.onscroll = show.bind ( demo.querySelector( "#section_2_demo" ) )
+
+window.onresize = function ( event ) {
+  main.onscroll()
+  section_1.onscroll()
+  section_2.onscroll()
+}
+
+document.addEventListener( "DOMContentLoaded", ready)
+
+function ready ( event ) {
+  main.dispatchEvent ( new Event ( "scroll" ))
+  section_1.dispatchEvent ( new Event ( "scroll" ))
+  section_2.dispatchEvent ( new Event ( "scroll" ))
 }
