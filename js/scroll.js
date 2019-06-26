f867efe0857aa9c76979a416da7b1560822d54c7
garevna <garevna@mail.ru>
2019-06-26 14:22:40 +0300

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -12,19 +12,19 @@ function show ( event ) {
     }
   )
   if ( event.target.id === "main" ) {
-          section_1.onscroll()
-          section_2.onscroll()
+          section_1.dispatchEvent ( new Event ( "scroll" ))
+          section_2.dispatchEvent ( new Event ( "scroll" ))
   }
 }
 
-main.onscroll = show.bind ( demo.querySelector( "#main_demo" ) )
+main.onscroll = show.bind ( demo.querySelector( "#main_demo" )  )
 section_1.onscroll = show.bind ( demo.querySelector( "#section_1_demo" ) )
 section_2.onscroll = show.bind ( demo.querySelector( "#section_2_demo" ) )
 
 window.onresize = function ( event ) {
-  main.onscroll()
-  section_1.onscroll()
-  section_2.onscroll()
+  main.dispatchEvent ( new Event ( "scroll" ))
+  section_1.dispatchEvent ( new Event ( "scroll" ))
+  section_2.dispatchEvent ( new Event ( "scroll" ))
 }
 
 document.addEventListener( "DOMContentLoaded", ready)
