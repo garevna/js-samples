4014b8708d12920d58eed43f58ee2fe8f0f63b1b
garevna <garevna@mail.ru>
2019-08-23 17:06:41 +0300

    Update index46.js

--- js/index46.js
+++ js/index46.js
@@ -1,20 +1,15 @@
 Notification.requestPermission(
     permission => permission === "granted" ? 
-        spawnNotification ( `Что вам следует знать о JS` ) : null
+        showNotification ( `Что вам следует знать о JS` ) : null
 )
-// document.body.appendChild ( document.createElement ( "img" ) ).src = "/js-samples/icons/communication-1.png"
-// document.body.appendChild ( document.createElement ( "img" ) ).src = "/js-samples/images/js-ico.png"
-    
-function spawnNotification( message ) {
-    var options = {
-//         badge: "https://i.pinimg.com/236x/6e/87/1c/6e871c81e47c31c11cd62267a8595e1c--animated-icons-social-media-icons.jpg",
-//         icon: "/js-samples/icons/link.png",
-//         body: message,
-        image: "/js-samples/images/1.jpg"
-    }
-    var note = new Notification( "garevna", options )
-    console.log ( 'badge', note.badge )
-    console.log ( 'image', note.image )
-    console.log ( 'icon', note.icon )
-    console.log ( 'body', note.body )
+
+showNotification ( message ) {
+    var notification = new Notification( 'garevna', {
+        icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+        body: message
+    })
+}
+
+notification.onclick = function () {
+    window.open( 'https://github.com/garevna/js-course/wiki' )
 }
