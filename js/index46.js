92e11ecfc566d9ece4c77ee6435157e0827eeb80
garevna <garevna@mail.ru>
2019-08-23 19:01:46 +0300

    Update index46.js

--- js/index46.js
+++ js/index46.js
@@ -5,12 +5,23 @@ Notification.requestPermission(
 
 function showNotification ( message ) {
     let options = {
+        tag : "js-course",
         icon: 'https://garevna.github.io/js-course/images/my-photo.png',
         body: message
     }
     const notification = new Notification( 'garevna', options )
     notification.onclick = function ( event ) {
         window.open( 'https://github.com/garevna/js-course/wiki' )
+    }
+    notification.onshow = function ( event ) {
+        document.body.appendChild (
+            document.createElement ( "img" )
+        ).src = event.target.icon
+    }
+    notification.onerror = function ( event ) {
+        document.body.appendChild (
+            document.createElement ( "p" )
+        ).innerText = "Notifications are not alowed"
     }
 }
 
