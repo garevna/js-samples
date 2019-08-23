05840881b6eebab4b24683c8622337cfefd224d8
garevna <garevna@mail.ru>
2019-08-23 15:20:48 +0300

    Update index46.js

--- js/index46.js
+++ js/index46.js
@@ -7,7 +7,7 @@ Notification.requestPermission(
 function spawnNotification( message ) {
     var options = {
         body: message,
-        icon: "../icons/communication-1.png",
+        icon: "./js-samples/icons/communication-1.png",
         badge: "https://cdn.glitch.com/a4e0a9fd-ea7b-47cf-b52a-48fd6359c559%2Fmy-photo.png",
         image: "https://cdn.glitch.com/a4e0a9fd-ea7b-47cf-b52a-48fd6359c559%2Fsmoke-monkey.gif",
         requireInteraction: true
