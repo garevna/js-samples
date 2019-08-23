7789a907dcfdabf13f2d37d4ff5055b69f45601d
garevna <garevna@mail.ru>
2019-08-23 15:37:59 +0300

    Update index46.js

--- js/index46.js
+++ js/index46.js
@@ -3,12 +3,12 @@ Notification.requestPermission(
     permission => permission === "granted" ? 
         spawnNotification ( text ) : null
 )
-document.body.appendChild ( document.createElement ( "img" ) ).src = "/js-samples/icons/communication-1.png"
-document.body.appendChild ( document.createElement ( "img" ) ).src = "/js-samples/images/js-ico.png"
+// document.body.appendChild ( document.createElement ( "img" ) ).src = "/js-samples/icons/communication-1.png"
+// document.body.appendChild ( document.createElement ( "img" ) ).src = "/js-samples/images/js-ico.png"
     
 function spawnNotification( message ) {
     var options = {
-        image: "/js-samples/images/js-ico.png"
+        image: "https://politikus.ru/uploads/posts/2012-09/1347832924_trollface.png"
     }
     var n = new Notification( "garevna", options )
 }
