3824742ff35effc436b7827b59d4f445acbc4f5c
garevna <garevna@mail.ru>
2019-08-23 17:25:30 +0300

    Update index46.js

--- js/index46.js
+++ js/index46.js
@@ -5,18 +5,12 @@ Notification.requestPermission(
 
 function showNotification ( message ) {
     let options = {
-        icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+        icon: 'https://www.dofactory.com/Images/Box/javascript-transparent.png',
         body: message
     }
     const notification = new Notification( 'garevna', options )
     notification.onclick = function ( event ) {
         window.open( 'https://github.com/garevna/js-course/wiki' )
-    }
-    notification.onerror = function ( event ) {
-        document.body.appendChild (
-            document.createElement ( "p" )
-        ).innerHTML = `<em style='color: red'>Error</p>`
-        console.log ( event )
     }
 }
 
