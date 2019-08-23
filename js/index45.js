7e826de105151d56d5471685edab47ed418be371
garevna <garevna@mail.ru>
2019-08-23 14:17:17 +0300

    Update index45.js

--- js/index45.js
+++ js/index45.js
@@ -1,17 +1,16 @@
 if ( Notification.permission !== "granted" )
     Notification.requestPermission( permission => console.log ( permission ) )
-if ( Notification.permission !== "granted" ) return
+if ( Notification.permission === "granted" )
+    spawnNotification(
+        "Hello, students )",
+        "https://www.webfx.com/tools/emoji-cheat-sheet/graphics/emojis/wink.png",
+        "garevna"
+    )
 
 function spawnNotification( theBody, theIcon, theTitle ) {
     var options = {
         body: theBody,
         icon: theIcon
     }
-  var n = new Notification(theTitle, options)
+    var n = new Notification(theTitle, options)
 }
-
-spawnNotification(
-    "Hello, students )",
-    "https://www.webfx.com/tools/emoji-cheat-sheet/graphics/emojis/wink.png",
-    "garevna"
-) 
