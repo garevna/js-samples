437c958af82e4e689c6b1de75f7ff133737b9f6a
garevna <garevna@mail.ru>
2019-08-25 09:29:25 +0300

    Update index46.js

--- js/index46.js
+++ js/index46.js
@@ -1,4 +1,4 @@
-var messageWorker = new Worker("https://garevna.github.io/js-samples/js/notification-worker.js")
+const messageWorker = new Worker("./js-samples/js/notification-worker.js")
 messageWorker.postMessage( "Hello!" )
 alert( "Worker" )
 messageWorker.terminate()
