44090762660f0e3805df96a5b74f3e3670c7af4e
garevna <garevna@mail.ru>
2019-08-25 09:35:41 +0300

    Update notifications-worker.js

--- js/notifications-worker.js
+++ js/notifications-worker.js
@@ -1,9 +1,15 @@
 onmessage = function ( event ) {
     Notification.requestPermission(
-        permission => permission === "granted" ? 
+        permission => {
+            console.log ( "permission", permission )
+            console.log ( "message", event.data )
+            permission === "granted" ? 
             showNotification ( event.data ) : null
+        }
     )
 }
+
+console.log ( "worker is working..." )
 
 function showNotification ( message ) {
     let options = {
