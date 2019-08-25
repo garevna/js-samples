de9746c665f6374a2a9abf82b565ac065d0c1ef8
garevna <garevna@mail.ru>
2019-08-25 08:39:54 +0300

    Update index46.js

--- js/index46.js
+++ js/index46.js
@@ -1,12 +1,13 @@
 Notification.requestPermission(
     permission => permission === "granted" ? 
-        showNotification ( `Что вам следует знать о JS` ) : null
+        showNotification () : null
 )
 
 function showNotification ( message ) {
     let options = {
-        icon: 'https://garevna.github.io/js-course/images/my-photo.png',
-        body: message
+//         icon: 'https://garevna.github.io/js-course/images/my-photo.png',
+        image: "http://dreempics.com/img/picture/Jun/03/684c20c33d76082098a4e9c47efd1df3/3.jpg"
+//         body: message
     }
     const notification = new Notification( 'garevna', options )
     notification.onclick = function ( event ) {
