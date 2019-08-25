73febcbf2956863a96dc6c97bfa15c3c72d0b8f2
garevna <garevna@mail.ru>
2019-08-25 09:37:45 +0300

    Update notifications-worker.js

--- js/notifications-worker.js
+++ js/notifications-worker.js
@@ -14,7 +14,7 @@ console.log ( "worker is working..." )
 function showNotification ( message ) {
     let options = {
         icon: 'https://garevna.github.io/js-course/images/my-photo.png',
-        image: "https://bipbap.ru/wp-content/uploads/2017/09/1kgwYBuQw7t5etDwP9o1XE9H-640x410.jpeg"
+        image: "https://bipbap.ru/wp-content/uploads/2017/09/1kgwYBuQw7t5etDwP9o1XE9H-640x410.jpeg",
         body: message
     }
     const notification = new Notification( 'garevna', options )
