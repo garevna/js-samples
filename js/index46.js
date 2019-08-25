793a04c506ae75d3f09a1ae26292760289ef4cc3
garevna <garevna@mail.ru>
2019-08-25 09:18:06 +0300

    Update index46.js

--- js/index46.js
+++ js/index46.js
@@ -1,22 +1,2 @@
-Notification.requestPermission(
-    permission => permission === "granted" ? 
-        showNotification () : null
-)
-
-function showNotification ( message ) {
-    let options = {
-//         icon: 'https://garevna.github.io/js-course/images/my-photo.png',
-        "image": "https://peter-gribanov.github.io/serviceworker/Bubble-Nebula_big.jpg"
-//         body: message
-    }
-    const notification = new Notification( 'garevna', options )
-    notification.onclick = function ( event ) {
-        document.body.appendChild ( document.createElement ( "img" ) ).src = notification.image
-        window.open( 'https://github.com/garevna/js-course/wiki' )
-    }
-    notification.onerror = function ( event ) {
-        console.warn ( "Notifications are not allowed" )
-    }
-}
-
-
+var messageWorker = new Worker("notification-worker.js")
+messageWorker.postMessage( "Hello!" )
