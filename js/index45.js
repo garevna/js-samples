367c0acd83e38e4506017375cb8991536d6cc1b1
garevna <garevna@mail.ru>
2019-08-25 11:47:07 +0300

    Update index45.js

--- js/index45.js
+++ js/index45.js
@@ -1,16 +1,16 @@
-if ( Notification.permission !== "granted" )
-    Notification.requestPermission( permission => console.log ( permission ) )
-if ( Notification.permission === "granted" )
-    spawnNotification(
-        "Hello, students )",
-        "https://www.webfx.com/tools/emoji-cheat-sheet/graphics/emojis/wink.png",
-        "garevna"
-    )
+Notification.requestPermission( permission => showNotification ({
+    icon: "https://garevna.github.io/js-course/images/my-photo.png",
+    image: "https://cn.opendesktop.org/img/b/2/d/5/cfd19f550736133723633e53ba0b05def2e4.jpg",
+    body: "Hello, students!",
+    title: "garevna"
+}) )
 
-function spawnNotification( theBody, theIcon, theTitle ) {
-    var options = {
-        body: theBody,
-        icon: theIcon
-    }
-    var n = new Notification(theTitle, options)
+function showNotification ( data ) {
+    const notification = new Notification( data.title, {
+        icon: data.icon,
+        image: data.image,
+        body: data.message,
+        requireInteraction: true,
+        silent: true
+    })
 }
