26c15d5c54053d5cd4b385a1903ab02d9df84154
garevna <garevna@mail.ru>
2019-08-25 11:13:59 +0300

    Update index46.js

--- js/index46.js
+++ js/index46.js
@@ -1,4 +1,22 @@
-const messageWorker = new Worker("./js/notifications-worker.js")
-messageWorker.postMessage( "Hello!" )
-alert( "new" )
-// messageWorker.terminate()
+const messageWorker = new Worker( "./js/notifications-worker.js" )
+
+messageWorker.postMessage({
+    icon: 'https://garevna.github.io/js-course/images/my-photo.png',
+    image: "https://garevna.github.io/js-course/images/columns.gif",
+    message: "Hello, students!",
+    title: "garevna"
+})
+
+const addElem = tag => document.body.appendChild (
+    document.createElement ( tag )
+}
+const addImage = url => addElem ( "img" ).src = url
+)
+
+messageWorker.onmessage = function ( event ) {
+    if ( event.data.type = "image" ) {
+      addImage ( event.data.image )
+      addImage ( event.data.icon )
+    } else addElem ( "p" ).innerText = event.data.text
+    messageWorker.terminate()
+}
