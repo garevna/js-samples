aa8133cb27ad51abbf6e977da59d1942c55b560e
garevna <garevna@mail.ru>
2019-08-25 08:42:24 +0300

    Update index46.js

--- js/index46.js
+++ js/index46.js
@@ -6,7 +6,7 @@ Notification.requestPermission(
 function showNotification ( message ) {
     let options = {
 //         icon: 'https://garevna.github.io/js-course/images/my-photo.png',
-        image: "http://dreempics.com/img/picture/Jun/03/684c20c33d76082098a4e9c47efd1df3/3.jpg"
+        image: "https://bipbap.ru/wp-content/uploads/2017/09/1kgwYBuQw7t5etDwP9o1XE9H-640x410.jpeg"
 //         body: message
     }
     const notification = new Notification( 'garevna', options )
