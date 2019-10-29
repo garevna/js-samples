fd1773fa9ff84650429b1ad16508185806ef700b
garevna <garevna@mail.ru>
2019-10-29 12:25:21 +0200

    Update mouseTail.js

--- snippets/mouseTail.js
+++ snippets/mouseTail.js
@@ -1,18 +1,19 @@
 function createMouseTail ( text ) {
-    const letters = text.split ( "" )
-    let currentMousePosition = { x:0, y:0 }
-    const elems = []
+    const letters = text.split ( "" );
+    let currentMousePosition = { x:0, y:0 };
+    let currentTime = new Date().getTime();
+    const elems = [];
 
-    const parentElem = document.createElement ( 'div' )
-    document.body.insertBefore ( parentElem, document.body.firstChild )
+    const parentElem = document.createElement ( 'div' );
+    document.body.insertBefore ( parentElem, document.body.firstChild );
     
-    const shadow = parentElem.attachShadow ( { mode: 'closed' } )
-    shadow.appendChild ( document.createElement ( 'div' ) ).className = "logo"
+    const shadow = parentElem.attachShadow ( { mode: 'closed' } );
+    shadow.appendChild ( document.createElement ( 'div' ) ).className = "logo";
     shadow.appendChild (
         ( () => {
                 var style = document.createElement ( 'style' )
                 style.textContent = `
-                   .mouseTailLetters {
+                   .letters {
                         position: fixed;
                         font-family: Arial;
                         transition: all 0.2s;
@@ -35,29 +36,36 @@ function createMouseTail ( text ) {
     )
     
     function appendLetter () {
-         const colors = [ '#f00','#ff0','#0ff','#f50','#0f0','#f0f','#fa0','#09b' ]
-         var elem = document.createElement( 'p' )
-         elem.className = "mouseTailLetters"
-         elem.innerHTML = letters [ elems.length ]
-         elem.style.color = colors [ elems.length < colors.length ?
-                           elems.length : elems.length - colors.length ]
-         shadow.appendChild ( elem )
-         elems.push ( elem )
+         const getColor = () => [ '#f00','#ff0','#0ff','#f50','#0f0','#f0f','#fa0','#09b' ][ Math.round ( Math.random() * 8 ) ];
+        elems.push (
+            Object.assign ( document.createElement( 'p' ), {
+                className: "letters",
+                innerHTML: letters [ elems.length ],
+                style: `color: ${getColor()}`
+            })
+        );
+        
+        shadow.appendChild ( elems [ elems.length - 1 ] );
     }
-    const test = event => Math.min (
+
+    const dist = event => Math.max (
         Math.abs ( currentMousePosition.x - event.clientX ),
-        Math.abs ( currentMousePosition.y - event.clientY ) 
-    )
+        Math.abs ( currentMousePosition.y - event.clientY )
+    );
+    
+    const test = event => dist( event ) > 10 && new Date().getTime() - currentTime > 50;
+    
     function redraw () {
         for ( var i = elems.length - 1; i > 0; i-- ) {
-            elems [i].style.top = elems [i-1].style.top
-            elems [i].style.left = elems [i-1].style.left
+            [ elems [i].style.top, elems [i].style.left ] = [ elems [i-1].style.top, elems [i-1].style.left ]
         }
-        elems [0].style.top = arguments[1] + "px"
-        elems [0].style.left = arguments[0] + "px"
+        elems [0].style.top = arguments[1] + "px";
+        elems [0].style.left = arguments[0] + "px";
     }
+    
     return function ( event ) {
-        if ( test ( event ) < 10 ) return
+        if ( !test ( event ) ) return
+        currentTime = new Date().getTime(); 
         currentMousePosition = { x: event.clientX, y: event.clientY }
         elems.length < letters.length ? appendLetter () : null
         requestAnimationFrame( redraw.bind ( null, event.clientX, event.clientY ) )
