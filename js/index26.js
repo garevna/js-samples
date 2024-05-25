835541e63472194d46ee55871a089f18f6daf77e
garevna <garevna@mail.ru>
2024-05-25 13:44:38 +0300

    Update index26.js

--- js/index26.js
+++ js/index26.js
@@ -1,29 +1,22 @@
-let mod = document.body.appendChild (
-  document.createElement ( "script" )
-)
-mod.type = "module"
+const mod = document.body
+  .appendChild(document.createElement('script'))
+mod.type = 'module'
 mod.textContent = `
-    import {showMessage} from './js/testESModules.js';
-    let promise = function ( message ) {
-        return new Promise (
-            function ( resolve, reject ) {
-                setInterval (
-                    () => resolve ( message ),
-                    10000
-                )
-            }
-        )
-    }
-    async function showMessages () {
-        const messages = [
-            "Hello, students!",
-            "Welcome to new age of ES Modules!",
-            "How are you feel about this?"
-        ]
-        for ( var item of messages )
-            await promise ( item )
-                .then ( showMessage )
-    }
+  import {showMessage} from './js/testESModules.js'
+  const promise = function (message) {
+    return new Promise(function (resolve, reject) {
+      setInterval(() => resolve(message), 10000)
+    })
+  }
 
-    showMessages ()
+  async function showMessages () {
+    const messages = [
+      'Hello, students!',
+      'Welcome to new age of ES Modules!',
+      'How are you feel about this?'
+    ]
+    for (const item of messages) await promise(item).then(showMessage)
+  }
+
+  showMessages()
 `
