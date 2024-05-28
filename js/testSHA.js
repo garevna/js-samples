2a5ececcf629aa70096a81cc934fbed29e75b814
garevna <garevna@mail.ru>
2024-05-28 06:34:15 +0300

    Update testSHA.js

--- js/testSHA.js
+++ js/testSHA.js
@@ -1,15 +1,15 @@
-﻿var hello = document.createElement ( 'div' )
-hello.style = `position: fixed; 
-                top: 15%; left: 15%; 
-                bottom: 15%; right: 15%; 
-                box-shadow: 10px 10px 16px #00000090; 
-                border: solid 0.5px #bbb; 
-                padding: 30px;
-                z-index: 300;
-                background-color: #eef;`
-document.body.appendChild ( hello )
-hello.innerHTML = "<h3>Success!</h3>"
-hello.innerHTML += "<p>file integrity test resuts with OK</p>"
-setTimeout ( () => {
-        document.body.removeChild ( hello )
-}, 4000 )
+const hello = document.createElement('div')
+hello.style = `
+  position: fixed; 
+  top: 15%; left: 15%; 
+  bottom: 15%; right: 15%; 
+  box-shadow: 10px 10px 16px #00000090; 
+  border: solid 0.5px #bbb; 
+  padding: 30px;
+  z-index: 300;
+  background-color: #eef;
+`
+document.body.appendChild(hello)
+hello.innerHTML = '<h3>Success!</h3>'
+hello.innerHTML += '<p>file integrity test resuts with OK</p>'
+setTimeout(() => document.body.removeChild(hello), 4000)
