13c4a6e2c2ad29d22817b4a52cc65db1a6115b0d
garevna <garevna@mail.ru>
2024-05-28 15:24:01 +0300

    Update index11.js

--- js/index11.js
+++ js/index11.js
@@ -1,16 +1,18 @@
 location.search = '?name=garevna,date=10.07.2018'
 
 function getSearchObject () {
-        var obj = {}
-        location.search.slice(1).split(',')
-                .map ( x => x.split('=') )
-                .map ( function ( item ) {
-                        this[ item [0] ] = item [1]
-        }, obj )
-        return obj
+  var obj = {}
+  location.search
+    .slice(1)
+    .split(',')
+    .map(x => x.split('='))
+    .map(function (item) {
+      this[item[0]] = item[1]
+    }, obj)
+  return obj
 }
 
 var searchObject = getSearchObject ()
-for ( var rec in searchObject ) {
-        document.body.innerHTML += `<p>${rec}: ${searchObject[rec]}</p>`
+for (var rec in searchObject) {
+  document.body.innerHTML += `<p>${rec}: ${searchObject[rec]}</p>`
 }
