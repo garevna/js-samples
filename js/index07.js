ba0317cdfb6d7adc82338fca6739f31dfd51f4b9
garevna <garevna@mail.ru>
2024-05-28 11:57:44 +0300

    Update index07.js

--- js/index07.js
+++ js/index07.js
@@ -1,19 +1,23 @@
-var sample = document.querySelector ( "input" )
-sample.oninput = getTextDigest
-sample.value = "Привет студентам A-level"
+var sample = Object.assign(document.querySelector('input'), {
+  oninput: getTextDigest,
+  value: 'Hi, students!'
+})
 
-var demo = document.createElement ( 'p' )
-document.body.appendChild ( demo )
+var demo = document.body.appendChild(document.createElement('p'))
 
-var script = document.createElement ( 'script' )
-script.src = "https://cdn.rawgit.com/chrisveness/crypto/4e93a4d/sha256.js"
-script.onload = getTextDigest
-script.onerror = function ( event ) {
-    console.error ( "Ошибка загрузки скрипта " + script.src )
-}
-document.head.appendChild ( script )
+var script = Object.assign(document.createElement('script'), {
+  src: 'https://cdn.rawgit.com/chrisveness/crypto/4e93a4d/sha256.js',
+  onload: getTextDigest,
+  onerror (event) {
+    console.error('Error loading script ' + script.src)
+  }
+})
+
+document.head.appendChild(script)
 
 function getTextDigest () {
-    demo.innerHTML = `<p>Text: <em>${sample.value}</em></p>
-        <p>Hash: ${Sha256.hash (sample.value)}</p>`
+  demo.innerHTML = `
+    <p>Text: <em>${sample.value}</em></p>
+    <p>Hash: ${Sha256.hash(sample.value)}</p>
+  `
 }
