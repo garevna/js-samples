dd953fac62fb754c373ca091895378a48ee2b0ff
garevna <garevna@mail.ru>
2024-05-29 11:08:53 +0300

    Update index12.js

--- js/index12.js
+++ js/index12.js
@@ -1,81 +1,98 @@
-var User = function ( name = "user",
-                      email = "user@sample.com",
-                      photo = User.getAvatar() ) {
-        this.name = name
-        this.email = email
-        this.photoURL = photo
-        this.info = ''
+const configs = {
+  host: 'https://garevna.github.io/js-samples/images',
+  avatars: [
+    'batman.png',
+    'cute-boy.png',
+    'spectacles-guy.png',
+    'avatar-cartoon.jpg',
+    '3d-human-avatar.jpg',
+    'avatar-01.png',
+    'girl-avatar.png',
+    'girl-avatar-1.png'
+  ],
+  admin: {
+    photoURL: 'https://garevna.github.io/js-samples/images/admin-avatar.gif',
+    name: 'Admin'
+  }
 }
-User.admin = {
-        photoURL: "https://i.pinimg.com/originals/3d/47/4f/3d474f82ff71595e8081f9a120892ae8.gif",
-        name: "admin"
+
+const User = function (name = 'user', email = 'user@sample.com', photoURL) {
+  Object.assign(this, {
+    name,
+    email,
+    photoURL: this.getAvatar(photoURL),
+    info: ''
+  })
 }
-User.getAvatar = function () {
-     return this.avatars.shift ()
+
+const createMessageBox = function (admin) {
+  const box = document.body.appendChild(document.createElement('div'))
+  const addElem = tag => box.appendChild(document.createElement(tag))
+  Object.assign(box, {
+    style: `
+      position: fixed;
+      bottom: 0;
+      right: 0;
+      width: 300px;
+      height: 200px;
+      overflow: auto;
+      border: 1px solid gray;
+      padding: 12px;
+      background-color: #000;
+    `,
+    picture: addElem('img'),
+    name: addElem('span'),
+    message: Object.assign(addElem('textarea'), { admin })
+  })
+
+  Object.assign(box.name, { style: 'font-weight: bold; color: #9ab; padding-left:10px;' })
+  Object.assign(box.picture.style, { width: '50px' })
+
+  Object.assign(box.message, {
+    placeholder: 'Message',
+    style: 'width: 100%; height: 100%;',
+    oninput (event) {
+      console.dir(this)
+      this.parentNode.querySelector('img').src = this.admin.photoURL
+      this.parentNode.querySelector('span').innerHTML = this.admin.name
+    }
+  })
+  return box
 }
-User.avatars = [
-     "https://pre00.deviantart.net/50f9/th/pre/i/2011/217/e/8/pikachu_2_by_nostalgiaattack-d45jd3i.png",
-     "https://cdn.diversityavatars.com/wp-content/uploads/2018/01/Vector-Smart-Object-5.png",
-     "https://cdn4.iconfinder.com/data/icons/user-avatar-flat-icons/512/User_Avatar-31-512.png",
-     "http://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-L3-icon.png",
-     "https://findicons.com/files/icons/1072/face_avatars/300/i05.png",
-     "http://www.iconarchive.com/download/i51043/hopstarter/halloween-avatars/Gomez.ico",
-     "http://icons.iconarchive.com/icons/hopstarter/halloween-avatars/256/Zombie-2-icon.png",
-     "https://vignette.wikia.nocookie.net/yogscast/images/8/8a/Avatar_Turps_2015.jpg"
+
+Object.assign(User.prototype, {
+  messageBox: createMessageBox(configs.admin),
+
+  host: configs.host,
+
+  avatars: configs.avatars,
+
+  getAvatar (photo) {
+    const name = photo || this.avatars[Math.round(Math.random() * (this.avatars.length - 1))]
+    return `${this.host}/${name}`
+  },
+
+  setActive () {
+    this.messageBox.picture.src = this.photoURL
+    this.messageBox.name.innerHTML = this.name
+  },
+
+  write (text) {
+    this.setActive()
+    this.messageBox.message.value = text
+  }
+})
+
+var users = [
+  new User('Andrew', null, 'batman.png'),
+  new User('Alex', 'alex@gmail.com', 'cute-boy.png'),
+  new User('Bob', 'bob777@gmail.com', 'spectacles-guy.png'),
+  new User('Frederic', 'frodo-888@gmail.com', 'avatar-cartoon.jpg'),
+  new User('Stephan', 'fima999@gmail.com', '3d-human-avatar.jpg')
 ]
-User.prototype.messageBox = ( function () {
-        var box = document.createElement ( 'div' )
-        document.body.appendChild ( box )
-        box.style = `
-                position: fixed;
-                bottom: 0;
-                right: 0;
-                width: 300px;
-                height: 200px;
-                overflow: auto;
-                border: 1px solid gray;
-                padding: 10px;
-                background-color: #000;
-        `
-        box.picture = box.appendChild ( document.createElement ( 'img' ) )
-        box.picture.style.width = "50px"
-        box.name = box.appendChild ( document.createElement ( 'span' ) )
-        box.name.style = "font-weight: bold; color: #9ab; padding-left:10px;"
-        box.message = box.appendChild ( document.createElement ( 'textarea' ) )
-        box.message.placeholder = "Сообщение"
-        box.message.oninput = function ( event ) {
-                event.target.parentNode.querySelector ( 'img' ).src = User.admin.photoURL
-                event.target.parentNode.querySelector ( 'span' ).innerHTML = User.admin.name
-        }
-        box.message.style = "width: 100%; height: 100%;"
-        return box
-})()
-User.prototype.write = function ( text ) {
-        this.messageBox.picture.src = this.photoURL
-        this.messageBox.name.innerHTML = this.name
-        this.messageBox.message.value = text
-}
-User.prototype.read = function () {
-        this.messageBox.picture.src = this.photoURL
-        this.messageBox.name.innerHTML = this.name
-        this.info = this.messageBox.message.value
-        console.log ( `${this.name} прочитал сообщение:\n${this.info}` )
-        this.messageBox.message.value = "OK"
-}
 
-var users = []
-users.push ( new User ( "Иван" ) )
-users.push ( new User ( 'Alex', "alex@gmail.com" ) )
-users.push ( new User ( 'Bob', "bob777@gmail.com" ) )
-users.push ( new User ( 'Dima', "dima888@gmail.com" ) )
-users.push ( new User ( 'Fima', "fima999@gmail.com" ) )
-
-var k = 1
-users.forEach ( 
-     function ( user ) {
-          setTimeout ( 
-               function () {
-                    user.write ( `Hello, I'm ${user.name}` )
-               }, 3000 * k++
-          )
-} )
+users.forEach(function (user, index) {
+  setTimeout(function () {
+    user.write(`Hello, I'm ${user.name}`)
+  }, 5000 * index)
+})
