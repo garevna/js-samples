88f2d979a0b070d0d21d00c4d53c88bd660f283c
garevna <19735284+garevna@users.noreply.github.com>
2024-06-07 12:27:40 +0300

    Update index16.js

--- js/index16.js
+++ js/index16.js
@@ -1,8 +1,8 @@
 var pictures = [
-  "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg",
-  "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg",
-  "https://stepupandlive.files.wordpress.com/2014/09/3d-animated-frog-image.jpg",
-  "http://pwtthemes.com/demo/hannari/wp-content/uploads/2013/03/unicorn-wallpaper.jpg"
+  'https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg',
+  'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg',
+  'https://stepupandlive.files.wordpress.com/2014/09/3d-animated-frog-image.jpg',
+  'http://pwtthemes.com/demo/hannari/wp-content/uploads/2013/03/unicorn-wallpaper.jpg'
 ]
 var messages = [
   'Everything is about nothing...',
@@ -13,11 +13,10 @@ var messages = [
 
 var picture = Object.assign(document.querySelector('img'), { width: 300 })
 var messagePlaceholder = document.querySelector('header > h1 > span')
-console.log(messagePlaceholder)
 
 window.onhashchange = function (event) {
-  var hash = 1 * location.hash.substr(1)
-  var hash = hash > 3 || hash < 0 ? 0 : hash
+  var hash = location.hash.slice(1) - 0
+  hash = hash > messages.length || hash < 0 ? 0 : hash
   location.hash = '#' + hash
   picture.src = pictures[hash]
   messagePlaceholder.innerText = messages[hash]
