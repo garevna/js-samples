7120c40c94b2d433558ba41dd656c0e30a4f6f88
garevna <19735284+garevna@users.noreply.github.com>
2024-06-07 13:38:25 +0300

    Update index27.js

--- js/index27.js
+++ js/index27.js
@@ -1,48 +1,46 @@
-var addElem = ( tagName, container ) =>
-    ( container ? container : document.body )
-        .appendChild (
-            document.createElement ( tagName )
-        )
+var addElem = (tagName, container = document.body) => container.appendChild(document.createElement(tagName))
 
-var timeOut = time => new Promise (
-    resolve => setTimeout ( () => resolve(), time )
-)
+var container = addElem('div')
+container.className = 'flex-container'
+var avatar = addElem('img', container)
+var userLogin = addElem('h4', container)
+var startButton = addElem('button', container)
 
-var container = addElem ( "div" )
-container.className = "flex-container"
-var startButton = addElem ( 'button', container )
-startButton.innerText = 'Крутить рулетку'
+var timeOut = time => new Promise(resolve => setTimeout(resolve, time))
+
+function show (photoURL, login) {
+  avatar.src = photoURL
+  avatar.style.display = 'block'
+  avatar.style.opacity = 1
+  userLogin.innerText = `winner: ${login}`
+  userLogin.style.display = 'block'
+}
+
+function getWinner () {
+  var winnwer = Math.round(Math.random() * 20000)
+  fetch(`https://api.github.com/users?since=${winnwer}`)
+    .then(response => response.json())
+    .then(users => show(users[0].avatar_url, users[0].login))
+}
+
+startButton.innerText = 'Spin the roulette'
 
 startButton.onclick = event => {
-    var winnwer = Math.round ( Math.random() * 20000 )
-    var user
-    event.target.style.display = "none"
-    function show( photoURL, login ) {
-        img.src = photoURL
-        user = addElem ( "h4", container )
-        user.innerText = `winner: ${login}`
-    }
-    var img = addElem( "img", container )
-    img.height = "200"
-    img.src = "https://thumbs.gfycat.com/LivelyObviousAnhinga-size_restricted.gif"
-    img.style.transition = "all 0.5s"
-    timeOut ( 2500 )
-        .then ( () => img.src = "https://thumbs.gfycat.com/OddWideHookersealion-small.gif" )
-    timeOut ( 1800 )
-        .then ( () => img.style.opacity = 0 )
-    timeOut ( 2500 )
-        .then ( () => img.style.opacity = 1 )
-    timeOut ( 2500 )
-        .then (
-            () => fetch ( `https://api.github.com/users?since=${winnwer}` )
-                      .then ( response => response.json()
-                          .then ( users => show ( users[0].avatar_url, users[0].login ) )
-                      )
-        )
-    timeOut ( 10000 )
-        .then ( () => {
-            img.remove()
-            user.remove()
-            event.target.style.display = "block"
-        })
+  event.target.style.display = 'none'
+
+  avatar.height = '200'
+  avatar.src = 'https://i.pinimg.com/originals/32/37/bf/3237bf1e172a6089e0c437ffd3b28010.gif'
+  avatar.style.transition = 'all 0.5s'
+  timeOut(2500)
+    .then(() => {
+      avatar.style.opacity = 0
+      getWinner()
+    })
+
+  timeOut(10000)
+    .then(() => {
+      avatar.style.display = 'none'
+      userLogin.style.display = 'none'
+      event.target.style.display = 'block'
+    })
 }
