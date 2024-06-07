9c9f744deacd2c77e3e4bee1576b8c1287c3d6f7
garevna <19735284+garevna@users.noreply.github.com>
2024-06-07 09:58:43 +0300

    Update index10.js

--- js/index10.js
+++ js/index10.js
@@ -1,45 +1,40 @@
-var LibraryBook = function ( _title = "Без названия", _year = "нет данных", _author = "нет данных" ) {
-        var title = _title
-        var year = _year
-        var author = _author
-        var readerName = null
-        var readerData = null
-        function giveTheBook ( client, data = new Date() ) {
-                readerName = client
-                readerData = data
-        }
-        this.getBookInfo = function () {
-                var text = readerName ? "выдана на руки" : "есть в наличии"
-                console.info ( `${author}, ${title} (${year}): ${text}` )
-        }
-        this.getTheBook = function ( client, data ) {
-                if ( readerName ) {
-                        this.getBookInfo ()
-                        return null
-                } else {
-                        giveTheBook ( client, data )
-                        return {
-                                title: title,
-                                year: year,
-                                author: author
-                        }
-                }
-        }
-        this.returnBook = function () {
-                readerName = null
-                readerData = null
-        }
+var LibraryBook = function (_title = 'Untitled', _year = 'no data', _author = 'no data') {
+  var title = _title
+  var year = _year
+  var author = _author
+  var readerName = null
+  var readerData = null
+  function giveTheBook (client, data = new Date()) {
+    readerName = client
+    readerData = data
+  }
+  this.getBookInfo = function () {
+    var text = readerName ? 'handed out' : 'are available'
+    console.info(`${author}, ${title} (${year}): ${text}`)
+  }
+  this.getTheBook = function (client, data) {
+    if (readerName) {
+      this.getBookInfo()
+      return null
+    } else {
+      giveTheBook(client, data)
+      return { title, year, author }
+    }
+  }
+  this.returnBook = function () {
+    readerName = null
+    readerData = null
+  }
 }
 
-var books = []
-books [0] = new LibraryBook ( "Война и мир", "1995", "Лев Толстой" )
-books [1] = new LibraryBook ( "Отцы и дети", "1998", "Тургенев" )
-books [3] = new LibraryBook ( "Кобзарь", "2005", "Тарас Шевченко" )
-
-console.log ( books )
+var books = [
+  new LibraryBook('Oliver Twist', 1838, 'Charles Dickens'),
+  new LibraryBook('Don Juan', 1819, 'Lord Byron'),
+  new LibraryBook('Cabbages and Kings', 1904, 'O. Henry'),
+]
 
 books [0].getBookInfo()
-books [0].getTheBook( "Иван Сидоренко", new Date ( 2018, 6, 25 ) )
+books [0].getTheBook('Stephan Milly', new Date(2018, 6, 25))
 books [0].getBookInfo()
 books [0].returnBook()
 books [0].getBookInfo()
