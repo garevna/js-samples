289cf5619f7e6ad7040028d9108594c5831d8a7b
garevna <19735284+garevna@users.noreply.github.com>
2024-09-02 11:36:29 +0300

    Update notifications-worker.js

--- js/notifications-worker.js
+++ js/notifications-worker.js
@@ -1,28 +1,28 @@
-onmessage = function ( event ) {
-    showNotification ( event.data )
+onmessage = function (event) {
+  showNotification(event.data)
 }
 
-function showNotification ( data ) {
-    const notification = new Notification( data.title, {
-        icon: data.icon,
-        image: data.image,
-        body: data.message,
-        requireInteraction: true,
-        silent: true
-    } )
+function showNotification (data) {
+  const notification = new Notification(data.title, {
+    icon: data.icon,
+    image: data.image,
+    body: data.message,
+    requireInteraction: true,
+    silent: true
+  })
     
-    notification.onshow = function ( event ) {
-        postMessage ({
-            messageType: "images",
-            image: notification.image,
-            icon: notification.icon
-        })
-    }
+  notification.onshow = function (event) {
+    postMessage({
+      messageType: 'images',
+      image: notification.image,
+      icon: notification.icon
+    })
+  }
     
-    notification.onclose = function ( event ) {
-        postMessage ({
-            messageType: "text",
-            text: "I'm worker. Notification has been closed"
-        })
-    }
+  notification.onclose = function (event) {
+    postMessage({
+      messageType: 'text',
+      text: 'I\'m worker. Notification has been closed.'
+    })
+  }
 }
